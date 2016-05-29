d5a4fcfb3250237a5604ead87615d43dde1d3708
michal-j <michal.jaciubek@gmail.com>
2016-05-29 18:25:21 +0200

    backup before wordpress

---
+++ koken/storage/configuration/database.php
@@ -0,0 +1,10 @@
+<?php
+	return array(
+		'driver' => 'mysqli',
+		'hostname' => 'jcbk.one.mysql',
+		'database' => 'jcbk_one',
+		'username' => 'jcbk_one',
+		'password' => 'mike12',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
---
+++ koken/storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return '1269ce26a6beb6899e85ce520cd9b321';
